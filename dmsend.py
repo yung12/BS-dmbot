ef6268250784284c949e2fd40e088c173b1e813a
yung12 <66030697+yung12@users.noreply.github.com>
2020-09-12 18:42:27 +0900

    Update dmsend.py

--- dmsend.py
+++ dmsend.py
@@ -1,7 +1,3 @@
-
-# 봉순#6959 : MASS DM BOT SOURCE
-
-
 import discord
 import asyncio
 import datetime
@@ -11,7 +7,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 정상적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    game = discord.Game('흠샵 서버관리중')
     await client.change_presence(status=discord.Status.online, activity=game)
 
 #/dm {할말}로 전체DM 전송
@@ -23,15 +19,16 @@ async def on_message(message):
                 pass
             else:
                 try:
+                    await asyncio.sleep(8)
                     msg = message.content[4:]
                     #메시지 관리권한이 있을시 사용가능
                     if message.author.guild_permissions.manage_messages:
                         embed = discord.Embed(color=0x1DDB16, timestamp=message.created_at)
-                        embed.add_field(name="★★제목★★", value=msg, inline=True)
-                        embed.set_footer(text="★맨 밑에 들어갈 내용★")
+                        embed.add_field(name="흠샵.com", value=msg, inline=True)
+                        embed.set_footer(text="HMM SHOP공지")
                         await i.send(embed=embed)
                 except:
                     pass
 
 
-client.run('★★봇토큰★★')
+client.run('NzUzOTkxNjYwMTczNTI1MTIy.X1uPdA.PD4SgIaZBGtFQUd7U-vn5VH445g')
